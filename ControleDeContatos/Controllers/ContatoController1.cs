a39bed8b4fd8e25c40d27a46f07467b8b111cc83
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-01 23:09:41 -0300

    Adicionando layout

---
+++ ControleDeContatos/Controllers/ContatoController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
