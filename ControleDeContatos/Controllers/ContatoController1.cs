2ff6676f4c28ecc5de5cfa425814882eb8dae092
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-02 21:25:17 -0300

    Adicionando Cadatro e listagem

--- ControleDeContatos/Controllers/ContatoController1.cs
+++ ControleDeContatos/Controllers/ContatoController1.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace ControleDeContatos.Controllers
 {
     public class ContatoController1 : Controller
     {
+        private readonly IContatoRepositorio _contatoRepositorio;
+        public ContatoController1(IContatoRepositorio contatoRepositorio)
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
         public IActionResult Index()
         {
-            return View();
+           List<ContatoModel> contatos = _contatoRepositorio.BuscarTodos();
+            return View(contatos);
         }
         public IActionResult Criar()
         {
@@ -20,6 +29,12 @@ namespace ControleDeContatos.Controllers
         public IActionResult ApagarConfirmacao()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato) 
+        {
+            _contatoRepositorio.Adicionar(contato);
+            return RedirectToAction("Index");
         }
     }
 }
