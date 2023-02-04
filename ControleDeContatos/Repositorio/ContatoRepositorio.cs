b76a62db85cb072648d709914d64873d031087bd
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-03 22:36:46 -0300

    Adicionando Editar e delete

--- ControleDeContatos/Repositorio/ContatoRepositorio.cs
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -12,6 +12,10 @@ namespace ControleDeContatos.Repositorio
         {
             _bancoContext = bancoContext;
         }
+        public ContatoModel ListarPorId(int id)
+        {
+            return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+        }
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
@@ -23,6 +27,31 @@ namespace ControleDeContatos.Repositorio
             return contato;
         }
 
-       
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if (contatoDB == null) throw new System.Exception("Houve um erro na atualização do contato");
+
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email= contato.Email;
+            contatoDB.Celular = contato.Celular;
+
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel contatoDB = ListarPorId(id);
+
+            if (contatoDB == null) throw new System.Exception("Houve um erro na delição do contato");
+
+            _bancoContext.Contatos.Remove(contatoDB);
+            _bancoContext.SaveChanges();
+            return true;
+        }
     }
 }
