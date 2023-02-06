6859e30ed2d64abf070e56d286ea415d6bf78de7
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-05 21:31:04 -0300

    Adicionando mensagens de erro

--- ControleDeContatos/Controllers/ContatoController1.cs
+++ ControleDeContatos/Controllers/ContatoController1.cs
@@ -43,8 +43,14 @@ namespace ControleDeContatos.Controllers
         [HttpPost]
         public IActionResult Criar(ContatoModel contato) 
         {
-            _contatoRepositorio.Adicionar(contato);
-            return RedirectToAction("Index");
+            if(ModelState.IsValid)
+            {
+                _contatoRepositorio.Adicionar(contato);
+                return RedirectToAction("Index");
+            }
+
+            return View(contato);
+           
         }
 
         [HttpPost]
--- ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -1,12 +1,17 @@
-﻿namespace ControleDeContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Digite o nome do contato")]
         public string Nome { get; set; }
-
+        [Required(ErrorMessage ="Digite o E-mail")]
+        [EmailAddress(ErrorMessage ="E-mail invalido")]
         public string Email { get; set;}
-
+        [Required(ErrorMessage ="Digite o celular")]
+        [Phone(ErrorMessage ="O celular informado não é valido")]
         public string Celular { get; set; }
     }
 }
