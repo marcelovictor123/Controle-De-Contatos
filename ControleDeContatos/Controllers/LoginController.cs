5a65204e079317b278be79893c407b019873c777
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-15 17:57:14 -0300

    Adicionando tela de login

---
+++ ControleDeContatos/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControleDeContatos.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    if(loginModel.Login == "adm" && loginModel.Senha == "123")
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    TempData["MensagemErro"] = $"Usuario e/ou senha invalido(s). Tente novamente";
+
+
+                }
+                return View("Index");
+
+            }
+            catch(Exception erro) 
+            {
+                TempData["MensagemErro"] = $"Ops, erro ao realizar login.{erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
