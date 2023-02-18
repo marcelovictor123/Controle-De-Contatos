6a6172f0ffd09857ff8b17d73326992e4da44006
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-18 20:03:57 -0300

    Criptografando a senha

--- ControleDeContatos/Repositorio/UsuarioRepositorio.cs
+++ ControleDeContatos/Repositorio/UsuarioRepositorio.cs
@@ -29,6 +29,7 @@ namespace ControleDeContatos.Repositorio
         public UsuarioModel Adicionar(UsuarioModel usuario)
         {
             usuario.DataDeCadastro = DateTime.Now;
+            usuario.SetSenhaHash();
             _bancoContext.Usuarios.Add(usuario);
             _bancoContext.SaveChanges();
             return usuario;
